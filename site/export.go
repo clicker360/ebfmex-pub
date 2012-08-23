e8f4a3c6701a25d9b8521a8549f389f454a7c1d3
Tom <thomas@clicker360.com>
2012-08-23 15:06:19 -0500

    funcion de export con empresas (para pruebas)

--- site/export.go
+++ site/export.go
@@ -8,6 +8,7 @@ import (
     "html/template"
     "time"
     "model"
+    //"fmt"
 )
 
 type Cta struct {
@@ -24,6 +25,69 @@ type Cta struct {
         UsuarioInt              string
         CodigoCfm               string
         Status                  bool
+}
+
+type Empresa struct {
+        IdEmp           string
+        Folio           int32
+        RFC                     string
+        Nombre          string
+        RazonSoc        string
+        DirCalle        string
+        DirCol          string
+        DirEnt          string
+        DirMun          string
+        DirCp           string
+        NumSuc          string
+        OrgEmp          string
+        OrgEmpOtro      string
+        OrgEmpReg       string
+        //Entidades     []Entidad
+        Url                     string
+        Benef           int
+        PartLinea       int
+        ExpComer        int
+        Desc            string
+        FechaHora       time.Time
+        Status          bool
+}
+
+type CtaEmpresa struct {
+        Folio                   int32
+        Nombre                  string
+        Apellidos               string
+        Puesto                  string
+        Email                   string
+        EmailAlt                string
+        Pass                    string
+        Tel                             string
+        Cel                             string
+        FechaHora               time.Time
+        UsuarioInt              string
+        CodigoCfm               string
+        Status                  bool
+        IdEmp           string
+        FolioE           int32
+        RFC                     string
+        NombreE          string
+        RazonSoc        string
+        DirCalle        string
+        DirCol          string
+        DirEnt          string
+        DirMun          string
+        DirCp           string
+        NumSuc          string
+        OrgEmp          string
+        OrgEmpOtro      string
+        OrgEmpReg       string
+        //Entidades     []Entidad
+        Url                     string
+        Benef           int
+        PartLinea       int
+        ExpComer        int
+        Desc            string
+        FechaHoraE       time.Time
+        StatusE          bool
 }
 
 func init() {
@@ -52,6 +116,9 @@ func registroCsv(w http.ResponseWriter, r *http.Request) {
         return
     }
 
+
+	w.Header().Set("Content-Type", "text/plain")
+
 	for _, value := range regdata {
 		q2 := datastore.NewQuery("Empresa").Ancestor(value.Key(c)).Limit(50)
 		for cursor := q2.Run(c); ; {
@@ -61,20 +128,51 @@ func registroCsv(w http.ResponseWriter, r *http.Request) {
 				break
 			}
 			// llenar linea de csv
-
+			ce := CtaEmpresa {
+        			Folio: value.Folio,
+			        Nombre: value.Nombre,
+			        Apellidos: value.Apellidos,
+			        Puesto: value.Puesto,
+			        Email: value.Email,
+			        EmailAlt: value.EmailAlt,
+			        Pass: value.Pass,
+			        Tel: value.Tel,
+			        Cel: value.Cel,
+			        FechaHora: value.FechaHora,
+			        UsuarioInt: value.UsuarioInt,
+			        CodigoCfm: value.CodigoCfm,
+			        Status: value.Status,
+			        IdEmp: e.IdEmp,
+			        FolioE: e.Folio,
+			        RFC: e.RFC,
+			        NombreE: e.Nombre,
+			        RazonSoc: e.RazonSoc,
+			        DirCalle: e.DirCalle,
+			        DirCol: e.DirCol,
+			        DirEnt: e.DirEnt,
+			        DirMun: e.DirMun,
+			        DirCp: e.DirCp,
+			        NumSuc: e.NumSuc,
+			        OrgEmp: e.OrgEmp,
+			        OrgEmpOtro: e.OrgEmpOtro,
+			        OrgEmpReg: e.OrgEmpReg,
+			        Url: e.Url,
+			        Benef: e.Benef,
+			        PartLinea: e.PartLinea,
+			        ExpComer: e.ExpComer,
+			        Desc: e.Desc,
+			        FechaHoraE: e.FechaHora,
+			        StatusE: e.Status,
+			}
+			//fmt.Fprintf(w, "%s", value.Nombre)
+			cuentasCsvTpl.Execute(w, ce)
 		}
 
-		empresas := make([]model.Empresa, 0, 100)
-		q2.GetAll(c, &empresas)
-		datacsv := value empresas
-        //return &empresas
 	}
 
-        w.Header().Set("Content-Type", "text/plain")
     //if err := registrosCsvTpl.Execute(w, regdata); err != nil {
-	if err := cuentasCsvTpl.Execute(w, datacsv); err != nil {
-	return
-    }
+//	return
+    //}
 }
 
 var exportTpl = template.Must(template.ParseFiles("templates/export.html"))
