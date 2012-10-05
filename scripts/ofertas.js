75d01eff354308e1494cb1421328152ed06fc201
Docotor <retinadiseno@gmail.com>
2012-10-04 20:55:03 -0500

    ofertas.js

--- scripts/ofertas.js
+++ scripts/ofertas.js
@@ -15,18 +15,6 @@ $(document).ready(function() {
 	$("#cards").click(function () { $("#allcards").toggle("slow"); });  
 	$("#ONL").click(function () { $("#onlineURL").toggle("slow"); });  
 	$("#SUC").click(function () { $("#sucursales").toggle("slow"); });  
-	
-/*	$('#saveproges').click(function() {
-  $('#slideForm').animate({
-    left: -760
-  }, 1000, function() {
-     Animation complete.
-  });
-});*/
-	
-	
-	
-	
 	$("#enviar").validationEngine({promptPosition : "topRight", scroll: false});
 	$("#enviardata").validationEngine({promptPosition : "topRight", scroll: false});
 	var $pic = $("#pic");
