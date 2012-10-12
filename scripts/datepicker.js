7ada114892865d3a7cd662d6131ed1e455c679d9
Docotor <retinadiseno@gmail.com>
2012-10-11 20:23:52 -0500

    globos de avisos

--- scripts/datepicker.js
+++ scripts/datepicker.js
@@ -17,3 +17,47 @@ function getTheDate(elem){
 		untogleCalendar();
 	return false;
 }
+$(document).ready(function() { 
+$("#sugerencia").mouseover(function(event){
+	var elmn = $("#slug1");
+	var posicion = $(this).position();
+	var theleft= posicion.left + $(this).width();
+	var thetop =posicion.top;
+  	elmn.css('left', theleft+40);
+	elmn.css('top', thetop-100);
+	elmn.toggle("fast");    
+   });
+   
+$("#sugerencia").mouseout(function() { 
+   $("#lsug1").toggle("fast");
+   });
+
+$("#dater").mouseover(function(event){
+	var elmn = $("#slug2");
+	var posicion = $(this).position();
+	var theleft= posicion.left + $(this).width();
+	var thetop =posicion.top;
+	elmn.css('left', theleft+20);
+	elmn.css('top', thetop-30);
+	elmn.toggle("fast");
+   });
+   
+$("#dater").mouseout(function() { 
+ $("#slug2").toggle("fast");
+   });
+   
+$("#worder").mouseover(function(event){
+	var elmn = $("#slug3");
+	var posicion = $(this).position();
+	var theleft= posicion.left + $(this).width();
+	var thetop =posicion.top;
+	elmn.css('left', theleft+20);
+	elmn.css('top', thetop-30);
+	elmn.toggle("fast");
+   });
+   
+$("#worder").mouseout(function() { 
+ $("#slug3").toggle("fast");
+   });
+});
+
