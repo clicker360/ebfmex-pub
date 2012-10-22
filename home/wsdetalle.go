78147ca7547803b9448f021834e69e92544b0e8a
ahuezo <ahuezo@clicker360.com>
2012-10-22 03:45:06 -0500

    Se agregan webservices para home, detalle oferta, micrositio y sucursales

---
+++ home/wsdetalle.go
@@ -0,0 +1,62 @@
+package home
+
+import (
+    "appengine"
+    "appengine/memcache"
+	"encoding/json"
+    "net/http"
+    "model"
+    "time"
+)
+
+type detalle struct {
+	IdEmp		string		`json:"idemp"`
+	IdOft		string		`json:"idoft"`
+	IdCat		int			`json:"idcat"`
+	Oferta		string		`json:"oferta"`
+	Empresa		string		`json:"empresa"`
+	Descripcion	string		`json:"descripcion"`
+	Enlinea     bool		`json:"enlinea"`
+	Url         string		`json:"url"`
+	BlobKey		appengine.BlobKey	`json:"imgurl"`
+}
+
+
+func init() {
+    http.HandleFunc("/wsdetalle", ShowOfDetalle)
+}
+
+func ShowOfDetalle(w http.ResponseWriter, r *http.Request) {
+	var timetolive = 900 //seconds
+	c := appengine.NewContext(r)
+	var b []byte
+	var d detalle
+	if item, err := memcache.Get(c, "d_"+r.FormValue("id")); err == memcache.ErrCacheMiss {
+		oferta, _ := model.GetOferta(c, r.FormValue("id"))
+		d.IdEmp = oferta.IdEmp
+		d.IdOft = oferta.IdOft
+		d.IdCat = oferta.IdCat
+		d.Oferta = oferta.Oferta
+		d.Empresa = oferta.Empresa
+		d.Descripcion = oferta.Descripcion
+		d.Enlinea = oferta.Enlinea
+		d.Url = oferta.Url
+		d.BlobKey = oferta.BlobKey
+
+		b, _ = json.Marshal(d)
+		item := &memcache.Item{
+			Key:   "d_"+r.FormValue("id"),
+			Value: b,
+			Expiration: time.Duration(timetolive)*time.Second,
+		}
+		if err := memcache.Add(c, item); err == memcache.ErrNotStored {
+			c.Errorf("Memcache.Add d_idoft : %v", err)
+		}
+	} else {
+		//c.Infof("memcache retrieve d_idoft : %v", r.FormValue("id"))
+		b = item.Value
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(b)
+}
+
---
+++ home/wsmicrositio.go
@@ -0,0 +1,57 @@
+package home
+
+import (
+    "appengine"
+    "appengine/memcache"
+	"encoding/json"
+    "net/http"
+    "model"
+    "time"
+)
+
+type micrositio struct {
+	IdEmp		string	`json:"idemp"`
+	IdImg		string	`json:"idimg"`
+	Name		string	`json:"name"`
+	Desc		string	`json:"desc"`
+	Url         string	`json:"url"`
+	Sp1         string	`json:"facebook"`
+	Sp2         string	`json:"twitter"`
+}
+
+func init() {
+    http.HandleFunc("/wsmicrositio", ShowMicrositio)
+}
+
+func ShowMicrositio(w http.ResponseWriter, r *http.Request) {
+	var timetolive = 1800 //seconds
+	c := appengine.NewContext(r)
+	var b []byte
+	var m micrositio
+	if item, err := memcache.Get(c, "m_"+r.FormValue("id")); err == memcache.ErrCacheMiss {
+		imgo := model.GetLogo(c, r.FormValue("id"))
+		m.IdEmp = imgo.IdEmp
+		m.IdImg = imgo.IdImg
+		m.Name = imgo.Name
+		m.Desc = imgo.Desc
+		m.Url = imgo.Url
+		m.Sp1 = imgo.Sp1
+		m.Sp2 = imgo.Sp2
+
+		b, _ = json.Marshal(m)
+		item := &memcache.Item{
+			Key:   "m_"+r.FormValue("id"),
+			Value: b,
+			Expiration: time.Duration(timetolive)*time.Second,
+		}
+		if err := memcache.Add(c, item); err == memcache.ErrNotStored {
+			c.Errorf("Error memcache.Add m_idoft : %v", err)
+		}
+	} else {
+		//c.Infof("memcache retrieve m_idoft : %v", r.FormValue("id"))
+		b = item.Value
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(b)
+}
+
