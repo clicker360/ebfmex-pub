b2f71ab855150dab3ca6c09a24eff8d3a01afcc7
ahuezo <ahuezo@clicker360.com>
2012-10-23 17:11:52 -0500

    Se agrega paginación a webservice de ofertas por empresa

--- home/wsofxe.go
+++ home/wsofxe.go
@@ -7,6 +7,7 @@ import (
     "net/http"
 	"sortutil"
     "model"
+	"strconv"
     "time"
 )
 
@@ -26,7 +27,16 @@ func init() {
 
 func ShowEmpOfertas(w http.ResponseWriter, r *http.Request) {
 	var timetolive = 1800 //seconds
+	var batch = 12 // tamaño de pagina
 	c := appengine.NewContext(r)
+	var page int
+	page,_ = strconv.Atoi(r.FormValue("pagina"))
+	page = page-1
+	if page < 1 {
+		page = 0
+	}
+	wsbatch := make([]wsoferta, 0, batch)
+	offset := page*batch
 	var b []byte
 	if item, err := memcache.Get(c, "ofxe_"+r.FormValue("id")); err == memcache.ErrCacheMiss {
 		emofs := model.ListOf(c, r.FormValue("id"))
@@ -41,18 +51,34 @@ func ShowEmpOfertas(w http.ResponseWriter, r *http.Request) {
 			wsofs[i].BlobKey = v.BlobKey
 		}
 		sortutil.AscByField(wsofs, "Oferta")
-		b, _ = json.Marshal(wsofs)
+		jb, _ := json.Marshal(wsofs)
 		item := &memcache.Item{
 			Key:   "ofxe_"+r.FormValue("id"),
-			Value: b,
+			Value: jb,
 			Expiration: time.Duration(timetolive)*time.Second,
 		}
 		if err := memcache.Add(c, item); err == memcache.ErrNotStored {
 			c.Errorf("Error memcache.Add ofxe_idemp : %v", err)
 		}
+
+		// se pagina la respuesta
+		if(offset <= len(*emofs)-batch) {
+			b, _ = json.Marshal(wsofs[offset:offset+batch])
+		} else {
+			b, _ = json.Marshal(wsofs[offset:])
+		}
 	} else {
 		//c.Infof("memcache retrieve sucs_idemp : %v", r.FormValue("id"))
-		b = item.Value
+		// se pagina la respuesta
+		if err := json.Unmarshal(item.Value, &wsbatch); err != nil {
+			c.Errorf("Unmarshaling wsbatch item: %v", err)
+		}
+		// se pagina la respuesta
+		if(offset <= len(wsbatch)-batch) {
+			b, _ = json.Marshal(wsbatch[offset:offset+batch])
+		} else {
+			b, _ = json.Marshal(wsbatch[offset:])
+		}
 	}
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
